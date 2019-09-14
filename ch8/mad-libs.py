aa070111b7c769a5bc32c5f87e09d1011a0918b5
Nick C <ncommella@gmail.com>
2019-09-14 15:39:24 -0700

    Desc and ToDos

--- ch8/mad-libs.py
+++ ch8/mad-libs.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+#mad-libs.py - Reads a text file, uses user input to create a mad lib to display
+#and create a new text file.
+#Usage: madlibs.py <filename> - runs and uses <filename> as the mad lib source file
+
+# TODO: Get name of Story text file from argument
+    # TODO: Check if no file named, and prompt user on use
+
+# TODO: Parse through source for keywords
+    # TODO: get user input to replace keyword
+
+# TODO: display final mad lib and save new file
