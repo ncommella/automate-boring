bfde11148f00f0a0b27eb23d0c7fe7aa19b28514
dev <ncommella@gmail.com>
2019-09-30 12:22:56 -0700

    Implemented user prompts for file-prefix.py

---
+++ ch9/file-prefix.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+# file-prefix.py - Write a program that finds all files with a given prefix,
+# such as spam001.txt, spam002.txt, and so on, in a single folder and locates
+# any gaps in the numbering (such as if there is a spam001.txt and spam003.txt
+# but no spam002.txt). Have the program rename all the later files to close this gap.
+
+# As an added challenge, write another program that can insert gaps into
+# numbered files so that a new file can be added.
+
+import os
+
+def update_files(directory, prefix):
+    for folder, subdirectory, filenames in os.walk(directory):
+        for fn in filenames:
+            #!!!! Look at glob and fnmatch modules
+    # TODO: Walk through directory
+    # TODO: search for files containing prefix
+        # TODO: analyze numbering, looking for gaps in numbering
+            # TODO: renumber all files after gap
+
+# Prompt user for information
+print("Please enter an option:\n")
+print("UPDATE - Update the numbering of files in a directory")
+print("NEW - Create a gap in numbered files for a new file\n")
+
+choice = input()
+
+while True:
+    if choice.lower() == "update":
+        # Get searchDirectory from user
+        searchDirectory = input("Enter a directory to search:\n")
+        while not os.path.exists(searchDirectory):
+            searchDirectory = input('Search directory does not exist. Please try again:\n')
+
+        # Get searchPrefix from user
+        searchPrefix = input("Enter a file prefix to search for:\n")
+
+        update_files(searchDirectory, searchPrefix)
+        break
+    elif choice.lower() == "new":
+        # Get searchDirectory from user
+        searchDirectory = input("Enter a directory to search:\n")
+        while not os.path.exists(searchDirectory):
+            searchDirectory = input('Search directory does not exist. Please try again:\n')
+
+        # Get searchPrefix from user
+        searchPrefix = input("Enter a file prefix to search for:\n")
+
+        print('New gap created!')
+        break
+    else:
+        choice = input("Not a valid option. Please try again:\n")
