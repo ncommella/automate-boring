760cc45a0e98b40180ba55f58c4c3242818bb66f
Nick C <ncommella@gmail.com>
2019-10-02 18:04:17 -0700

    Implemented update feature of file-prefix.py

--- ch9/file-prefix.py
+++ ch9/file-prefix.py
@@ -8,19 +8,47 @@
 # As an added challenge, write another program that can insert gaps into
 # numbered files so that a new file can be added.
 
-import os, glob
+import os, glob, shutil
 
-def update_files(directory, prefix):
+# Takes in a user-defined directory and file prefix and returns a sorted array of the
+# files in the directory that contain the prefix and three digts afterwards,
+# independent of file extension
+def get_glob(directory, prefix):
     path = os.path.join(directory, prefix)
 
-    fileArray = glob.glob('{}???.txt'.format(path))
+    fileArray = glob.glob('{}[0-9][0-9][0-9].???'.format(path))
     fileArray.sort()
-    print(fileArray)
-            
-    # TODO: Walk through directory
-    # TODO: search for files containing prefix
-        # TODO: analyze numbering, looking for gaps in numbering
-            # TODO: renumber all files after gap
+    return fileArray
+
+#Takes in a sorted array of absolute path strings to numbered files and checks for gaps
+# in the numbering, then fixes said gaps by renaming files
+# TODO: Find a way to reduce number of lists used? Also, rename 'Arrays' to 'lists'
+def update_numbering(array):
+    intArray = []
+    for filename in array:
+        # get the three numbers from all filenames, and convert to an integer array
+        dotIndex = filename.find('.')
+        intArray.append(int(filename[(dotIndex - 3):dotIndex]))
+
+    # find index of first item that isn't in the correct order (gapIndex)
+    for i in range(len(intArray)):
+        if intArray[i] != (i + 1):
+            gapIndex = i
+            break
+
+    # create array of correctly numbered and formatted strings
+    stringArray = intArray[gapIndex:]
+    for i in range(len(stringArray)):
+        stringArray[i] = str(i + 3).zfill(3)
+
+    renameArray = array[gapIndex:]
+
+    # (dotIndex -3):dotIndex of renameArray contains old file numbering
+    # this builds the new filename for each file in the renameArray and renames it
+    for i in range(len(renameArray)):
+        newName = renameArray[i][:dotIndex - 3] + stringArray[i] + renameArray[i][dotIndex:]
+        print("Renaming '{}' to '{}'".format(renameArray[i], newName))
+        shutil.move(renameArray[i], newName)
 
 # Prompt user for information
 print("Please enter an option:\n")
@@ -39,7 +67,8 @@ while True:
         # Get searchPrefix from user
         searchPrefix = input("Enter a file prefix to search for:\n")
 
-        update_files(searchDirectory, searchPrefix)
+        updateGlob = get_glob(searchDirectory, searchPrefix)
+        update_numbering(updateGlob)
         break
 
     elif choice.lower() == "new":
